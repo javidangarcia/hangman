727357c69f783297e5a0ac39ad818b88ce345341
Javier Garcia <javierdangarcia@gmail.com>
2022-06-10 00:04:22 -0400

    added buttons & letters

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 import os
+import math
 
 # WINDOW PROPERTIES
 pygame.init()
 WIDTH, HEIGHT = 800, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman")
+BUTTON_FONT = pygame.font.SysFont("comicsans", 35)
 
 # COLORS
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # LOADING IMAGES
 images = []
@@ -16,25 +19,57 @@ for num in range(7):
     image = pygame.transform.scale(pygame.image.load(os.path.join("assets", "hangman" + str(num) + ".png")), (216, 209))
     images.append(image)
 
+# BUTTON VARIABLES
+RADIUS = 20
+GAP = 15
+letters = []
+start_x = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+start_y = 370
+a = 65
+for i in range(26):
+    x = start_x + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = start_y + ((i // 13) * (GAP + RADIUS * 2))
+    letter = a + i
+    letters.append([x, y, chr(letter), True])
+
 # GAME VARIABLES
 FPS = 60
 clock = pygame.time.Clock()
-run = True
-hangman_status = 6
+hangman_status = 1
 
-while run:
-    clock.tick(FPS)
+# GAME FUNCTIONS
+def draw():
     WINDOW.fill(WHITE)
-    WINDOW.blit(images[hangman_status], (150, 100))
+
+    for letter in letters:
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(WINDOW, BLACK, (x, y), RADIUS, 3)
+            text = BUTTON_FONT.render(ltr, 1, BLACK)
+            WINDOW.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
+
+    WINDOW.blit(images[hangman_status], (40, 80))
     pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            position = pygame.mouse.get_pos()
-            print(position)
+def main():
+    run = True
+    while run:
+        clock.tick(FPS)
+        draw()
 
-pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for letter in letters:
+                    x, y, ltr, visible = letter
+                    if visible:
+                        distance = math.sqrt( math.pow(x - mouse_x, 2) + math.pow(y - mouse_y, 2) )
+                        if distance < RADIUS:
+                            letter[3] = False
 
+    pygame.quit()
+
+main()
 
